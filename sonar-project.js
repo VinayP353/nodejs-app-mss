a9e5434a9a1030b6e4d05e7b9fec3e3db48f4d16
VinayP353 <151072961+VinayP353@users.noreply.github.com>
2024-12-27 16:51:40 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.111.23.2:9000/',
+  serverUrl: 'http://34.229.79.116:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_8b5bf1e9057b32ea49a542206eadec00089c43cf',
+	    'sonar.login': 'sqa_25cfd076fece8e77db332d964d4ca272f20b0468',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
